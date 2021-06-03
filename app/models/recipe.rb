ed330d8ac0490bebf026c9e468441eebb5f9af6a
Jenny Kim <kimj1905@gmail.com>
2021-06-03 16:28:51 -0400

    commit tree 967901c5517699df6034949884f0c40a0a611376

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,10 @@
-#Placeholder for a model
+class Recipe < ActiveRecord::Base
+    
+end
+
+# <% @recipes.each do |recipe| %>
+#     # <%= <a href='/recipes/#{recipe.id}'>recipe.name</a> %>
+#     <%= recipe.name %>
+#     <%= recipe.ingredients %>
+#     <%= recipe.cook_time %>
+#     <% end %>
